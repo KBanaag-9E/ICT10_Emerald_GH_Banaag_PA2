eb1ba33afd71ed648dd98540cb232f9722e589cb
KBanaag-10E <banaag.kj@obmontessori.edu.ph>
2026-01-22 08:17:02 +0800

    Refactor students_classification function and remove julias_answer

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 from pyscript import display, document
 
-# First example
-def julias_answer(e):
-    document.getElementById("output").innerHTML = ''
-
-    response = document.getElementById("input1").value.lower()
-
-    if response == 'yes':
-        display(f"Kian will be her valentine", target="output")
-    elif response == 'no':
-        display(f"Kian will be rejected/heart broken", target="output")
-    elif response == 'maybe':
-        display(f"Don't give up try again!", target="output")
-    else:
-        display(f"Invalid input", target="output")
-
-# Second example
 def students_classification(e):
     document.getElementById("result").innerHTML = ''
 
     grade = float(document.getElementById("number1").value)
 
-    if grade >= 95:
+    if grade >= 95 and grade <= 100:
         display(f"Bergamo 1", target="result")
     elif grade >= 91 and grade < 95:
         display(f"Bergamo 2", target="result")
